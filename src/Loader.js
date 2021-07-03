a4c3d9abfe3e702d1801fcb9cee802ff3cf5ddbf
suprunov <rocknroll2418@gmail.com>
2021-07-03 18:21:15 +0300

    react_1

---
+++ src/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () =>
+    <div style={{margin: '.5rem', justifyContent: 'center', display: 'flex'}}>
+        <div className="lds-heart">
+            <div></div>
+        </div>
+    </div>
